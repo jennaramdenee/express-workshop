270c7da3d5abda18880a76edf0c4265d25ae0451
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-28 11:29:27 +0000

    Created first home route

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express')
 var app = express()
 
+app.get("/", function(request, response){
+  response.send("Yay Node Girls!")
+})
+
+// app.get("/node", function(request, response){
+//   response.send("Gotta love Node!")
+// })
+//
+// app.get("/")
+
 app.listen(3000, function(){
   console.log("Server is listening on port 3000. Ready to accept requests!")
 })
